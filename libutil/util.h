b5d10520e2deb68db013b7c75579ce7e7140ca55
Sergey Zolotykh <szolotykh88@gmail.com>
2022-11-28 15:47:39 -0800

    Added CTimerInterval calass

--- libutil/util.h
+++ libutil/util.h
@@ -7,13 +7,32 @@
 #ifndef LIBUTIL_UTIL_H
 #define LIBUTIL_UTIL_H
 
-#include <string>
+#include <thread>
+#include <mutex>
+#include <functional>
 
 namespace vsutil
     {
-    class CQueue
+    class CTimerInterval
         {
+        public:
+            CTimerInterval(std::function<void()> action, unsigned int sleep_interval);
+            ~CTimerInterval();
 
+        public:
+            void Stop();
+
+        private:
+            bool isStopping();
+            void fProcess();
+
+        private:
+            unsigned int m_sleep_interval; // ms
+            std::function<void()> m_action;
+
+            std::thread m_thd;
+            std::mutex m_StopMutex;
+            bool m_bShutdown;
         };
     }
 
